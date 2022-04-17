ccff12c0bf909df2903ec1fe1820a2f81fa5aba5
docoloter <84177215+docoloter@users.noreply.github.com>
2022-04-17 13:04:41 +0300

    Update game.py

--- game.py
+++ game.py
@@ -3,3 +3,24 @@
 #3
 #4
 #5
+#docoloter:
+back = (200,255,255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width,win_height))
+window.fill(back)
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player("racket.png",30,200,4,50,150)
+racket2 = Player("racket.png",520,200,4,50,150)
+ball = GameSprite('tenis_ball.png',200,200,4,50,50)
+
+font.init()
+font = font.Font("calibri.ttf",35)
+lose1 = font.render('PLAYER 1 LOSE!',True,(180,0,0))
+lose2 = font.render('PLAYER 2 LOSE!',True,(180,0,0))
