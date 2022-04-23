356345adc405401ff1abe0b819d6cc1e05398a1f
Andrey1297 <84615172+Andrey1297@users.noreply.github.com>
2022-04-23 07:18:32 -0200

    Update game.py

--- game.py
+++ game.py
@@ -85,5 +85,23 @@ while game:
     display.update()
     clock.tick(FPS)
 #5
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
 #docoloter:
 
