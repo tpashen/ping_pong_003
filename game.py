8b707456f5cdf44568cc771494f9394aa3f24c91
Andrey1297 <84615172+Andrey1297@users.noreply.github.com>
2022-05-01 07:17:29 -0200

    Update game.py

--- game.py
+++ game.py
@@ -3,14 +3,21 @@ class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (width, height))
-        self.speed = player_speed
-
+        self.speed = player_speed   
+        
+        
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-#2
+#
+
+#музыка
+#mixer.init()
+#mixer.music.load('')
+#mixer.music.play()
+
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
